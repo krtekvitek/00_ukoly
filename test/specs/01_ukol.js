dc4d45d44f7bbb045fcef1f81fa4d0ae54a30750
Tereza Jochimova <terka.jochim@gmail.com>
2020-11-16 14:23:47 +0100

    Nastaveni2

--- test/specs/01_ukol.js
+++ test/specs/01_ukol.js
@@ -1,15 +1,7 @@
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
-        browser.url(`https://the-internet.herokuapp.com/login`);
-        browser.pause(5000);
-        $('#username').setValue(ahoj);
-        browser.pause(5000);
-        $('#password').setValue(ahoj);
-        $('button[type="submit"]').click(); 
-        browser.pause(5000);
-        expect($('#flash')).toBeExisting();
-        expect($('#flash')).toHaveTextContaining(
-            'You logged into a secure area!');
+describe('', () => {
+    // TEST-CASE
+    it('', () => {
+
+      
     });
 });
-
